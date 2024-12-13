3a6d8bac4f1ee23fefdb3702fa1c3afdcaedaa95
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-13 00:35:16 -0500

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import { property } from "three/webgpu";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -40,10 +39,10 @@ export default tseslint.config(
         "warn",
         {
           allowConstantExport: true,
-          // For react three fiber
-          "react/no-unknown-property": "off"
         },
       ],
+      // For react three fiber
+      "react/no-unknown-property": "off"
     },
   },
 );
