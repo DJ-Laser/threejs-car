28d8e70661e628292708cb3add62da70a7de64c3
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-16 10:06:14 -0500

    shadows might make it easier to see whats going on?

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import { Car } from "./components/Car";
 
 function AppInternal() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
+    <Canvas camera={{ position: [3, 3, 3] }} shadows>
       <Physics debug>
         <ambientLight intensity={0.2} />
-        <directionalLight position={[0, 5, -5]} />
+        <directionalLight position={[-5, 5, 0]} castShadow />
         <Car />
         <RigidBody type="fixed" position={[0, -2, 0]}>
-          <mesh>
+          <mesh receiveShadow>
             <boxGeometry args={[100, 2, 100]} />
+            <meshStandardMaterial />
+          </mesh>
+          <mesh castShadow position={[0, 3, -3]}>
+            <boxGeometry args={[1, 2, 1]} />
             <meshStandardMaterial />
           </mesh>
         </RigidBody>
